541b4298f5f5be553d63d55a779a3ab245131d95
zuzexx <tjasa.zilavec@gmail.com>
2022-11-01 18:01:42 +0100

    .

--- src/routes/NewbieChallenges/ProfileCardComponent.vue
+++ src/routes/NewbieChallenges/ProfileCardComponent.vue
@@ -16,11 +16,13 @@ export default {
 };
 </script>
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap");
 .main {
   background-image: url(@/assets/ProfileCardComponent/bg-pattern-top.svg),
     url(@/assets/ProfileCardComponent/bg-pattern-bottom.svg);
   background-repeat: no-repeat, no-repeat;
   background-position: right 52vw bottom 37vh, left 49vw top 50vh;
   width: 100%;
+  font-family: "Kumbh Sans", sans-serif;
 }
 </style>
