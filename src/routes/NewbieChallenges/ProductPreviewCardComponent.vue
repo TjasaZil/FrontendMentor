a2aa94d70ed78957d088eabe5712ecdddb8d5c3c
zuzexx <tjasa.zilavec@gmail.com>
2022-11-06 21:21:35 +0100

    .

--- src/routes/NewbieChallenges/ProductPreviewCardComponent.vue
+++ src/routes/NewbieChallenges/ProductPreviewCardComponent.vue
@@ -2,10 +2,10 @@
   <section
     class="main w-screen h-screen flex flex-col justify-center items-center bg-product-cream"
   >
-    <section
+    <div
       class="bg-white w-10/12 mx-auto h-[611px] mobilem:w-[343px] flex flex-col justify-start items-center rounded-lg tablet:w-[600px] tablet:h-[450px] tablet:flex-row text-left"
     >
-      <picture x-intersect="$el.classList.add('swingIn')">
+      <picture>
         <source
           media="(min-width:768px)"
           srcset="
@@ -20,9 +20,10 @@
           src="@/assets/ProductPreviewCardComponent/image-product-mobile.jpg"
           alt="a square bottle with a gold lid and a label"
           class="rounded-t-lg tablet:rounded-t-none tablet:rounded-l-lg tablet:h-[450px] tablet:w-[300px]"
+          x-intersect="$el.classList.add('swingIn')"
         />
       </picture>
-      <section
+      <div
         class="space-y-5 flex flex-col justify-center items-center tablet:w-[311px]"
       >
         <h2
@@ -37,32 +38,32 @@
         >
           Gabrielle Essence Eau De Parfum
         </h1>
-        <div
+        <p
           role="paragraph"
           class="mx-auto w-10/12 text-[13px] leading-5 tablet:text-[15px] font-medium text-product-dark-grayish-blue tablet:leading-6"
           x-intersect="$el.classList.add('fadeIn')"
         >
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL.
-        </div>
-        <section
+        </p>
+        <div
           class="flex flex-row justify-start items-center mx-auto w-10/12 space-x-8"
         >
-          <div
+          <p
             role="paragraph"
             x-intersect="$el.classList.add('fadeLeftRight')"
             class="text-product-dark-cyan font-bold text-3xl big-cash"
           >
             $149.99
-          </div>
-          <div
+          </p>
+          <p
             role="paragraph"
             x-intersect="$el.classList.add('fadeRightLeft')"
             class="text-product-dark-grayish-blue line-through font-medium text-[13px]"
           >
             $169.99
-          </div>
-        </section>
+          </p>
+        </div>
         <button
           class="bg-product-dark-cyan w-10/12 rounded-lg text-center max-w-[295px] text-[13px] tablet:text-[15px] hover:bg-product-very-dark-blue"
           x-intersect="$el.classList.add('fadeIn')"
@@ -79,8 +80,8 @@
             <p>Add to Cart</p>
           </div>
         </button>
-      </section>
-    </section>
+      </div>
+    </div>
   </section>
 </template>
 
