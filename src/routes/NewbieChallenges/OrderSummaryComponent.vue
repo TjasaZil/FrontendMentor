1ee623c901c1ee1e0a2127221ed10dd8b9aa171c
zuzexx <tjasa.zilavec@gmail.com>
2022-11-08 07:09:55 +0100

    .OrderSummaryComponent mobile+ style

--- src/routes/NewbieChallenges/OrderSummaryComponent.vue
+++ src/routes/NewbieChallenges/OrderSummaryComponent.vue
@@ -3,23 +3,23 @@
     class="main w-full h-screen flex flex-col justify-center items-center"
   >
     <div
-      class="bg-white rounded-xl h-[567px] w-11/12 mobilem:w-[327px] mobilel:w-9/12 mobilel:max-w-[450px] flex flex-col justify-start items-center text-center"
+      class="bg-white rounded-3xl h-[567px] w-11/12 mobilem:w-[327px] mobilel:w-9/12 mobilel:max-w-[450px] mobilel:h-[697px] flex flex-col justify-start items-center text-center space-y-5 mobilel:space-y-7"
     >
       <img
         src="@/assets/OrderSumaryComponent/illustration-hero.svg"
         alt="hero image"
         data-testid="hero-image-test"
-        class="rounded-t-xl"
+        class="rounded-t-3xl"
         x-intersect="$el.classList.add('swingIn')"
       />
       <h1
-        class="font-black text-order-dark-blue w-10/12 mx-auto"
+        class="mobilel:pt-5 font-black text-order-dark-blue w-9/12 mx-auto text-2xl mobilel:text-3xl capitalize"
         x-intersect="$el.classList.add('fadeRightLeft')"
       >
         order summary
       </h1>
       <p
-        class="w-10/12 mx-auto"
+        class="w-9/12 mx-auto text-sm mobilel:text-base text-order-desaturated-blue leading-5 font-normal"
         data-testid="paragraph-text-test"
         x-intersect="$el.classList.add('fadeLeftRight')"
       >
@@ -27,27 +27,52 @@
         device anywhere you like!
       </p>
       <div
-        class="w-10/12 mx-auto flex justify-between items-center bg-order-very-pale-blue rounded-xl mt-5"
+        class="w-9/12 mobilem:max-w-[279px] mobilem:max-h-[81px] mobilel:max-w-[354px] mobilel:py-3 mx-auto flex justify-center items-center bg-order-very-pale-blue rounded-2xl mt-5"
         x-intersect="$el.classList.add('fadeIn')"
       >
-        <img
-          src="@/assets/OrderSumaryComponent/icon-music.svg"
-          alt="note icon"
-          class="w-7"
-          data-testid="note-icon-test"
-          x-intersect="$el.classList.add('swingIn')"
-        />
-        <div class="flex flex-col justify-center items-center">
-          <p x-intersect="$el.classList.add('fadeLeftRight')">annual plan</p>
-          <p x-intersect="$el.classList.add('fadeLeftRight')">$59.99/year</p>
+        <div
+          class="w-10/12 mobilem:w-[247px] mobilem:max-h-[48px] mobilel:w-[306px] mx-auto my-auto flex justify-between items-center"
+        >
+          <div class="flex justify-between items-center space-x-4">
+            <img
+              src="@/assets/OrderSumaryComponent/icon-music.svg"
+              alt="note icon"
+              class="w-12"
+              data-testid="note-icon-test"
+              x-intersect="$el.classList.add('swingIn')"
+            />
+            <div class="flex flex-col justify-center items-center">
+              <p
+                class="text-sm mobilel:text-base text-order-dark-blue font-black capitalize"
+                x-intersect="$el.classList.add('fadeLeftRight')"
+              >
+                annual plan
+              </p>
+              <p
+                class="text-sm mobilel:text-base text-order-desaturated-blue font-normal"
+                x-intersect="$el.classList.add('fadeLeftRight')"
+              >
+                $59.99/year
+              </p>
+            </div>
+          </div>
+
+          <p
+            class="font-black underline text-order-bright-blue text-sm mobilel:text-base capitalize hover:cursor-pointer hover:opacity-90 hover:no-underline"
+            x-intersect="$el.classList.add('fadeRightLeft')"
+          >
+            change
+          </p>
         </div>
-        <p x-intersect="$el.classList.add('fadeRightLeft')">change</p>
       </div>
-      <button class="w-10/12 mx-auto" x-intersect="$el.classList.add('fadeIn')">
+      <button
+        class="w-9/12 mx-auto text-sm mobilel:text-base py-4 text-white bg-order-bright-blue font-bold shadow-3xl rounded-2xl hover:cursor-pointer hover:opacity-90 mobilel:mt-5 hover:animate-pulse"
+        x-intersect="$el.classList.add('fadeIn')"
+      >
         Proceed to Payment
       </button>
       <p
-        class="w-10/12 mx-auto"
+        class="w-9/12 mx-auto capitalize text-order-desaturated-blue hover:cursor-pointer hover:text-order-dark-blue"
         x-intersect="$el.classList.add('fadeLeftRight')"
       >
         cancel order
