7d47a75ae71c49f745416431b66ea2ac70d52a98
zuzexx <tjasa.zilavec@gmail.com>
2022-11-08 06:29:04 +0100

    .OrderSummaryComponent animation made

--- src/routes/NewbieChallenges/OrderSummaryComponent.vue
+++ src/routes/NewbieChallenges/OrderSummaryComponent.vue
@@ -3,35 +3,55 @@
     class="main w-full h-screen flex flex-col justify-center items-center"
   >
     <div
-      class="bg-white rounded-xl h-[567px] w-11/12 mobilem:w-[327px] mobilel:w-9/12 mobilel:max-w-[450px] flex flex-col justify-start items-center"
+      class="bg-white rounded-xl h-[567px] w-11/12 mobilem:w-[327px] mobilel:w-9/12 mobilel:max-w-[450px] flex flex-col justify-start items-center text-center"
     >
       <img
         src="@/assets/OrderSumaryComponent/illustration-hero.svg"
         alt="hero image"
         data-testid="hero-image-test"
+        class="rounded-t-xl"
+        x-intersect="$el.classList.add('swingIn')"
       />
-      <h1>order summary</h1>
-      <p data-testid="paragraph-text-test">
+      <h1
+        class="font-black text-order-dark-blue w-10/12 mx-auto"
+        x-intersect="$el.classList.add('fadeRightLeft')"
+      >
+        order summary
+      </h1>
+      <p
+        class="w-10/12 mx-auto"
+        data-testid="paragraph-text-test"
+        x-intersect="$el.classList.add('fadeLeftRight')"
+      >
         You can now listen to millions of songs, audiobooks, and podcasts on any
         device anywhere you like!
       </p>
       <div
-        class="flex justify-between items-center bg-order-very-pale-blue rounded-xl mt-5"
+        class="w-10/12 mx-auto flex justify-between items-center bg-order-very-pale-blue rounded-xl mt-5"
+        x-intersect="$el.classList.add('fadeIn')"
       >
         <img
           src="@/assets/OrderSumaryComponent/icon-music.svg"
           alt="note icon"
           class="w-7"
           data-testid="note-icon-test"
+          x-intersect="$el.classList.add('swingIn')"
         />
         <div class="flex flex-col justify-center items-center">
-          <p>annual plan</p>
-          <p>$59.99/year</p>
+          <p x-intersect="$el.classList.add('fadeLeftRight')">annual plan</p>
+          <p x-intersect="$el.classList.add('fadeLeftRight')">$59.99/year</p>
         </div>
-        <p>change</p>
+        <p x-intersect="$el.classList.add('fadeRightLeft')">change</p>
       </div>
-      <button>Proceed to Payment</button>
-      <p>cancel order</p>
+      <button class="w-10/12 mx-auto" x-intersect="$el.classList.add('fadeIn')">
+        Proceed to Payment
+      </button>
+      <p
+        class="w-10/12 mx-auto"
+        x-intersect="$el.classList.add('fadeLeftRight')"
+      >
+        cancel order
+      </p>
     </div>
   </section>
 </template>
