105d759206532dd33d2ab04c3ae6185fae16a0da
zuzexx <tjasa.zilavec@gmail.com>
2022-11-10 20:18:16 +0100

    .NewsHomepage fully tested

--- src/routes/JuniorChallenges/NewsHomepage.vue
+++ src/routes/JuniorChallenges/NewsHomepage.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="main flex flex-col justify-center items-center laptop:h-screen w-full bg-news-off-white"
+    class="main flex flex-col text-black justify-center items-center laptop:h-screen w-full bg-news-off-white"
   >
     <!-- NAVIGATION -->
     <div
@@ -41,16 +41,126 @@
         <li v-for="nav in navs" :key="nav">{{ nav }}</li>
       </ul>
     </div>
+
+    <!-- BODY -->
+    <!--- TOP -->
+    <div
+      class="flex flex-col laptop:flex-row justify-center items-center w-full"
+    >
+      <div
+        class="laptop:flex laptop:flex-col laptop:justify-center laptop:items-center"
+      >
+        <picture>
+          <source
+            media="(min-width:768px)"
+            srcset="@/assets/NewsHomepage/image-web-3-desktop.jpg"
+          />
+          <source
+            media="(min-width:465px)"
+            srcset="@/assets/NewsHomepage/image-web-3-mobile.jpg"
+          />
+          <img
+            src="@/assets/NewsHomepage/image-web-3-mobile.jpg"
+            alt="hero image"
+            data-testid="hero-image-test"
+          />
+        </picture>
+
+        <div class="laptop:flex laptop:justify-center laptop:items-center">
+          <h1 data-testid="hero-heading-test">The Bright Future of Web 3.0?</h1>
+
+          <div
+            class="laptop:flex-col laptop:justify-center laptop:items-center"
+          >
+            <p>
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            <button>read more</button>
+          </div>
+        </div>
+      </div>
+      <!-- blog section -->
+      <div data-testid="blog-section-test">
+        <h2 data-testid="blog-section-heading">New</h2>
+        <h3 data-testid="blog-small-heading">Hydrogen VS Electric Cars</h3>
+        <p data-testid="blog-paragraph-test">
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </p>
+        <hr />
+        <h3 data-testid="blog-small-heading">The Downsides of AI Artistry</h3>
+        <p data-testid="blog-paragraph-test">
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+        <hr />
+        <h3 data-testid="blog-small-heading">Is VC Funding Drying Up?</h3>
+        <p data-testid="blog-paragraph-test">
+          Is Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </div>
+    </div>
+
+    <!-- BOTTOM -->
+
+    <div class="flex flex-col laptop:flex-row justify-center items-center">
+      <div
+        v-for="blog in blogs"
+        :key="blog"
+        class="flex laptop:flex-row justify-center items-center"
+      >
+        <div>
+          <img
+            :src="blog.img"
+            :alt="blog.alt"
+            data-testid="bottom-image-test"
+          />
+        </div>
+        <div class="flex flex-col justify-center items-center">
+          <div data-testid="number-test">{{ blog.number }}</div>
+          <div data-testid="small-bottom-heading">{{ blog.heading }}</div>
+          <div data-testid="small-bottom-paragraph">{{ blog.paragraph }}</div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import retroImage from "@/assets/NewsHomepage/image-retro-pcs.jpg";
+import laptopImage from "@/assets/NewsHomepage/image-top-laptops.jpg";
+import growthImage from "@/assets/NewsHomepage/image-gaming-growth.jpg";
 export default {
   name: "NewsHomepage",
   data() {
     return {
       open: false,
       navs: ["home", "new", "popular", "trending", "categories"],
+      blogs: [
+        {
+          img: retroImage,
+          alt: "retro PCs",
+          number: "01",
+          heading: "Reviving Retro PCs",
+          paragraph: "What happens when old PCs are given modern upgrades?",
+        },
+        {
+          img: laptopImage,
+          alt: "top laptops",
+          number: "02",
+          heading: "Top 10 Laptops of 2022",
+          paragraph: "Our best picks for various needs and budgets",
+        },
+        {
+          img: growthImage,
+          alt: "growth of gaming",
+          number: "03",
+          heading: "The Growth of Gaming",
+          paragraph: "How the pandemic has sparked fresh opportunities",
+        },
+      ],
     };
   },
 
--- tests/unit/routes/JuniorChallenges/NewsHomepage.test.js
+++ tests/unit/routes/JuniorChallenges/NewsHomepage.test.js
@@ -193,5 +193,227 @@ describe("NewsHomepage", () => {
         });
       });
     });
+    describe("Body Top", () => {
+      it("it has hero image", () => {
+        render(News);
+        const hero = screen.queryByTestId("hero-image-test");
+        expect(hero).toBeInTheDocument();
+      });
+      it("has visible hero image", () => {
+        render(News);
+        const hero = screen.queryByTestId("hero-image-test");
+        expect(hero).toBeVisible();
+      });
+      it("has hero image with alt attribute", () => {
+        render(News);
+        const hero = screen.queryByAltText("hero image");
+        expect(hero).toBeInTheDocument();
+      });
+      it("has hero image attribute alt", () => {
+        render(News);
+        const hero = screen.queryByAltText("hero image");
+        expect(hero).toHaveAttribute("src");
+      });
+      it("has hero heading", () => {
+        render(News);
+        const heading = screen.queryByTestId("hero-heading-test");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has hero heading with an appropriate text", () => {
+        render(News);
+        const heading = screen.queryByText("The Bright Future of Web 3.0?");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has appropriate paragraph in the hero", () => {
+        render(News);
+        const paragraph = screen.queryByText(
+          "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?"
+        );
+        expect(paragraph).toBeInTheDocument();
+      });
+      it("has button", () => {
+        render(News);
+        const button = screen.queryByRole("button");
+        expect(button).toBeInTheDocument();
+      });
+      it("has button with text read more", () => {
+        render(News);
+        const button = screen.queryByRole("button");
+        expect(button).toHaveTextContent("read more");
+      });
+      it("has a blog section", () => {
+        render(News);
+        const blog = screen.queryByTestId("blog-section-test");
+        expect(blog).toBeInTheDocument();
+      });
+      it("has 2 separators in blog section", () => {
+        render(News);
+        const separator = screen.queryAllByRole("separator");
+        expect(separator.length).toBe(2);
+      });
+      it("has blog section heading", () => {
+        render(News);
+        const heading = screen.queryByTestId("blog-section-heading");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has appropriate blog section heading text", () => {
+        render(News);
+        const heading = screen.queryByTestId("blog-section-heading");
+        expect(heading).toHaveTextContent("New");
+      });
+      it("has 3 blog headings", () => {
+        render(News);
+        const heading = screen.queryAllByTestId("blog-small-heading");
+        expect(heading.length).toBe(3);
+      });
+      describe("blog headings", () => {
+        it("has blog heading text Hydrogen VS Electric Cars", () => {
+          render(News);
+          const heading = screen.queryByText("Hydrogen VS Electric Cars");
+          expect(heading).toBeInTheDocument();
+        });
+        it("has a blog heading text The Downsides of AI Artistry", () => {
+          render(News);
+          const heading = screen.queryByText("The Downsides of AI Artistry");
+          expect(heading).toBeInTheDocument();
+        });
+        it("has a blog heading text Is VC Funding Drying Up?", () => {
+          render(News);
+          const heading = screen.queryByText("Is VC Funding Drying Up?");
+          expect(heading).toBeInTheDocument();
+        });
+      });
+      it("has 3 blog paragraphs", () => {
+        render(News);
+        const paragraph = screen.queryAllByTestId("blog-paragraph-test");
+        expect(paragraph.length).toBe(3);
+      });
+      describe("blog paragraphs", () => {
+        it("has blog paragraph text Will hydrogen-fueled cars ever catch up to EVs?", () => {
+          render(News);
+          const paragraph = screen.queryByText(
+            "Will hydrogen-fueled cars ever catch up to EVs?"
+          );
+          expect(paragraph).toBeInTheDocument();
+        });
+        it("has a blog paragraph text What are the possible adverse effects of on-demand AI image generation?", () => {
+          render(News);
+          const paragraph = screen.queryByText(
+            "What are the possible adverse effects of on-demand AI image generation?"
+          );
+          expect(paragraph).toBeInTheDocument();
+        });
+        it("has a blog paragraph text Is Private funding by VC firms is down 50% YOY. We take a look at what that means.", () => {
+          render(News);
+          const paragraph = screen.queryByText(
+            "Is Private funding by VC firms is down 50% YOY. We take a look at what that means."
+          );
+          expect(paragraph).toBeInTheDocument();
+        });
+      });
+    });
+    describe("Body Bottom", () => {
+      it("has 3 bottom images", () => {
+        render(News);
+        const image = screen.queryAllByTestId("bottom-image-test");
+        expect(image.length).toBe(3);
+      });
+      it("has image with alt retro PCs", () => {
+        render(News);
+        const image = screen.queryByAltText("retro PCs");
+        expect(image).toBeInTheDocument();
+      });
+      it("has retro pc image with src attribute", () => {
+        render(News);
+        const image = screen.queryByAltText("retro PCs");
+        expect(image).toHaveAttribute("src");
+      });
+      it("has image with alt top laptops", () => {
+        render(News);
+        const image = screen.queryByAltText("top laptops");
+        expect(image).toBeInTheDocument();
+      });
+      it("has top laptop image with src attribute", () => {
+        render(News);
+        const image = screen.queryByAltText("top laptops");
+        expect(image).toHaveAttribute("src");
+      });
+      it("has image with growth of gaming alt", () => {
+        render(News);
+        const image = screen.queryByAltText("growth of gaming");
+        expect(image).toBeInTheDocument();
+      });
+      it("has growth of gaming image with a src", () => {
+        render(News);
+        const image = screen.queryByAltText("growth of gaming");
+        expect(image).toHaveAttribute("src");
+      });
+      it("has 3 numbers", () => {
+        render(News);
+        const number = screen.queryAllByTestId("number-test");
+        expect(number.length).toBe(3);
+      });
+      it("has text 01", () => {
+        render(News);
+        const number = screen.queryByText("01");
+        expect(number).toBeInTheDocument();
+      });
+      it("has text 02", () => {
+        render(News);
+        const number = screen.queryByText("02");
+        expect(number).toBeInTheDocument();
+      });
+      it("has text 03", () => {
+        render(News);
+        const number = screen.queryByText("03");
+        expect(number).toBeInTheDocument();
+      });
+      it("has 3 small headings", () => {
+        render(News);
+        const heading = screen.queryAllByTestId("small-bottom-heading");
+        expect(heading.length).toBe(3);
+      });
+      it("has heading with text Reviving Retro PCs", () => {
+        render(News);
+        const heading = screen.queryByText("Reviving Retro PCs");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has heading with text Top 10 Laptops of 2022", () => {
+        render(News);
+        const heading = screen.queryByText("Top 10 Laptops of 2022");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has heading with text The Growth of Gaming", () => {
+        render(News);
+        const heading = screen.queryByText("The Growth of Gaming");
+        expect(heading).toBeInTheDocument();
+      });
+      it("has 3 paragraphs", () => {
+        render(News);
+        const heading = screen.queryAllByTestId("small-bottom-paragraph");
+        expect(heading.length).toBe(3);
+      });
+      it("gas paragraph with text What happens when old PCs are given modern upgrades?", () => {
+        render(News);
+        const paragraph = screen.queryByText(
+          "What happens when old PCs are given modern upgrades?"
+        );
+        expect(paragraph).toBeInTheDocument();
+      });
+      it("gas paragraph with text Our best picks for various needs and budgets", () => {
+        render(News);
+        const paragraph = screen.queryByText(
+          "Our best picks for various needs and budgets"
+        );
+        expect(paragraph).toBeInTheDocument();
+      });
+      it("gas paragraph with text How the pandemic has sparked fresh opportunities", () => {
+        render(News);
+        const paragraph = screen.queryByText(
+          "How the pandemic has sparked fresh opportunities"
+        );
+        expect(paragraph).toBeInTheDocument();
+      });
+    });
   });
 });
