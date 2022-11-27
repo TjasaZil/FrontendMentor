4c745a447003c2c79bbbbced472cf824b1d81222
zuzexx <tjasa.zilavec@gmail.com>
2022-11-27 09:45:40 +0100

    .MeetLandingPage is fully tested

--- src/routes/NewbieChallenges/MeetLandingPage.vue
+++ src/routes/NewbieChallenges/MeetLandingPage.vue
@@ -1,34 +1,90 @@
 <template>
-  <div class="main-div bg-meet-light text-meet-dark">
-    <div>logo image</div>
-    <div>
-      <div>hero image</div>
-      <div>text</div>
-      <div>buttons</div>
+  <div class="main-div bg-meet-light text-meet-dark h-screen">
+    <img
+      alt="Meet landing page logo"
+      src="@/assets/MeetLandingPage/logo.svg"
+      class="w-12"
+    />
+    <div class="laptop:hidden">
+      <div class="mobile-hero" data-testid="mobile-tablet-hero-image"></div>
+      <div>
+        <h1>Group Chat for Everyone</h1>
+        <p>
+          Meet makes it easy to connect with others face-to-face virtually and
+          collaborate across any device.
+        </p>
+      </div>
+      <div><button>Download v1.3</button><button>What is it?</button></div>
     </div>
-    <div class="hidden xl:flex">xl hero</div>
+    <div
+      class="hidden laptop:flex flex-row justify-center content-center w-full"
+    >
+      <div
+        class="desktop-left-hero"
+        data-testid="desktop-left-hero-image"
+      ></div>
+      <div>
+        <h1>Group Chat for Everyone</h1>
+        <p>
+          Meet makes it easy to connect with others face-to-face virtually and
+          collaborate across any device.
+        </p>
+      </div>
+      <div><button>Download v1.3</button><button>btn2</button></div>
+      <div
+        class="desktop-right-hero"
+        data-testid="desktop-right-hero-image"
+      ></div>
+    </div>
     <div>number 1</div>
-    <div>picture cluster</div>
     <div>
-      <div>top hjeader</div>
-      <div>header</div>
-      <div>text</div>
+      <img
+        v-for="image in images"
+        :key="image"
+        :src="image"
+        alt="People enjoying our services"
+      />
+    </div>
+    <div>
+      <h3>built for modern use</h3>
+      <h2>Smarter meetings all in one place</h2>
+      <p>
+        Send messages, share files, show your screen, and record your meetings —
+        all in one workspace. Control who can join with invite-only team access,
+        data encryption, and data export.
+      </p>
     </div>
     <div>number2</div>
 
-    <div>
-      <footer>
-        <div>heading</div>
-        <div>text</div>
-        <div>btn</div>
+    <div class="w-full mx-auto bg-meet-teal bg-opacity-40">
+      <footer
+        class="footer w-full mx-auto flex flex-col justify-center content-center bg-meet-teal"
+        data-testid="footer-image"
+      >
+        <h2>Experience more together</h2>
+        <p>
+          Stay connected with reliable HD meetings and unlimited one-on-one and
+          group video sessions.
+        </p>
+        <button>Download v1.3</button>
       </footer>
     </div>
   </div>
 </template>
 
 <script>
+import img1 from "@/assets/MeetLandingPage/desktop/image-woman-in-videocall.jpg";
+import img2 from "@/assets/MeetLandingPage/desktop/image-women-videochatting.jpg";
+import img3 from "@/assets/MeetLandingPage/desktop/image-men-in-meeting.jpg";
+import img4 from "@/assets/MeetLandingPage/desktop/image-man-texting.jpg";
 export default {
   name: "MeetLandingPage",
+
+  data() {
+    return {
+      images: [img1, img2, img3, img4],
+    };
+  },
 };
 </script>
 <style scoped>
@@ -36,5 +92,32 @@ export default {
 
 .main-div {
   font-family: "Red Hat Display", sans-serif;
+}
+.mobile-hero {
+  background-image: url("@/assets/MeetLandingPage/tablet/image-hero.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 100vw;
+  height: 100px;
+}
+.desktop-left-hero {
+  background-image: url("@/assets/MeetLandingPage/desktop/image-hero-left.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 100px;
+  width: 100px;
+}
+.desktop-right-hero {
+  background-image: url("@/assets/MeetLandingPage/desktop/image-hero-right.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 100px;
+  width: 100px;
+}
+.footer {
+  background-image: url("@/assets/MeetLandingPage/mobile/image-footer.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100px;
 }
 </style>
--- tests/unit/routes/NewbieChallenges/MeetLandingPage.test.js
+++ tests/unit/routes/NewbieChallenges/MeetLandingPage.test.js
@@ -6,22 +6,123 @@ import Meet from "@/routes/NewbieChallenges/MeetLandingPage.vue";
 describe("MeetLandingPage", () => {
   describe("layout", () => {
     describe("images", () => {
-      it("has appropriate logo image visible", () => {});
-      it("has appropriate tablet and mobila hero image", () => {});
-      it("has appropriate desktop hero images", () => {});
-      it("has appropriate footer image", () => {});
-      it("has 4 pictures in the middle", () => {});
+      it("has appropriate logo image visible", () => {
+        render(Meet);
+        const logo = screen.queryByAltText("Meet landing page logo");
+        expect(logo).toBeVisible();
+      });
+      it("has a src attribute on meet logo", () => {
+        render(Meet);
+        const src = screen.queryByAltText("Meet landing page logo");
+        expect(src).toHaveAttribute("src");
+      });
+      it("has appropriate tablet and mobile hero image", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("mobile-tablet-hero-image");
+        expect(hero).toBeInTheDocument();
+      });
+      it("has appropriate class on mobile/tablet hero", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("mobile-tablet-hero-image");
+        expect(hero).toHaveClass("mobile-hero");
+      });
+      it("has appropriate desktop left hero image", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("desktop-left-hero-image");
+        expect(hero).toBeInTheDocument();
+      });
+      it("has appropriate desktop right hero image", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("desktop-right-hero-image");
+        expect(hero).toBeInTheDocument();
+      });
+      it("has appropriate class on left desktop hero", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("desktop-left-hero-image");
+        expect(hero).toHaveClass("desktop-left-hero");
+      });
+      it("has appropriate class on right desktop hero", () => {
+        render(Meet);
+        const hero = screen.queryByTestId("desktop-right-hero-image");
+        expect(hero).toHaveClass("desktop-right-hero");
+      });
+      it("has appropriate footer image", () => {
+        render(Meet);
+        const footer = screen.queryByTestId("footer-image");
+        expect(footer).toHaveClass("footer");
+      });
+      it("has 4 pictures in the middle", () => {
+        render(Meet);
+        const image = screen.queryAllByAltText("People enjoying our services");
+        expect(image.length).toBe(4);
+      });
     });
     describe("text", () => {
-      it("has appropriate headings visible", () => {});
-      it("has appropriate paragraph text visible", () => {});
-      it("has appropriate numbers visible", () => {});
+      describe("has appropriate headings visible", () => {
+        it("Group Chat for Everyone", () => {
+          render(Meet);
+          const heading = screen.queryAllByText(/Group Chat for Everyone/i);
+          expect(heading.length).toBe(2);
+        });
+        it("built for modern use", () => {
+          render(Meet);
+          const heading = screen.queryByText(/built for modern use/i);
+          expect(heading).toBeVisible();
+        });
+        it("Smarter meetings all in one place", () => {
+          render(Meet);
+          const heading = screen.queryByText(
+            /Smarter meetings all in one place/i
+          );
+          expect(heading).toBeVisible();
+        });
+        it("Experience more together", () => {
+          render(Meet);
+          const heading = screen.queryByText(/Experience more together/i);
+          expect(heading).toBeVisible();
+        });
+      });
+      describe("has appropriate paragraph text visible", () => {
+        it("Meet makes it easy to connect with others face-to-face virtually and collaborate across any device.", () => {
+          render(Meet);
+          const text = screen.queryAllByText(
+            /Meet makes it easy to connect with others face-to-face virtually and collaborate across any device./i
+          );
+          expect(text.length).toBe(2);
+        });
+        it("Send messages, share files, show your screen, and record your meetings — all in one workspace. Control who can join with invite-only team access, data encryption, and data export.", () => {
+          render(Meet);
+          const text = screen.queryByText(
+            /Send messages, share files, show your screen, and record your meetings — all in one workspace. Control who can join with invite-only team access, data encryption, and data export./i
+          );
+          expect(text).toBeVisible();
+        });
+        it("Stay connected with reliable HD meetings and unlimited one-on-one and group video sessions.", () => {
+          render(Meet);
+          const text = screen.queryByText(
+            /Stay connected with reliable HD meetings and unlimited one-on-one and group video sessions./i
+          );
+          expect(text).toBeVisible();
+        });
+      });
     });
 
     describe("button", () => {
-      it("has 3 buttons", () => {});
-      it("has 2 download buttons", () => {});
-      it("has one what is it button", () => {});
+      it("has 3 buttons", () => {
+        render(Meet);
+        const btn = screen.queryAllByRole("button");
+        expect(btn.length).toBe(5);
+      });
+      it("has 3 download buttons", () => {
+        render(Meet);
+        const btn = screen.queryAllByText(/download v1.3/i);
+        expect(btn.length).toBe(3);
+      });
+      it("has one what is it button", () => {
+        render(Meet);
+        const btn = screen.queryByText(/what is it?/i);
+        expect(btn).toBeVisible();
+      });
     });
   });
 });
