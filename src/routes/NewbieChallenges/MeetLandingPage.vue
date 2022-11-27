1117fcb939b04d49ce63fc0c0736922974627b8b
zuzexx <tjasa.zilavec@gmail.com>
2022-11-27 16:22:44 +0100

    .layout MeetLandingPage

--- src/routes/NewbieChallenges/MeetLandingPage.vue
+++ src/routes/NewbieChallenges/MeetLandingPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="main-div bg-meet-light text-meet-dark w-full overflow-x-hidden">
+  <section
+    class="main-div bg-meet-light text-meet-dark w-full overflow-x-hidden"
+  >
     <!-- LOGO -->
     <img
       alt="Meet landing page logo"
@@ -7,9 +9,12 @@
       class="w-[119px] mx-auto pt-24"
     />
     <!-- MOBILE/TABLET HERO -->
-    <div class="laptop:hidden w-full mx-auto mt-11">
-      <div class="mobile-hero" data-testid="mobile-tablet-hero-image"></div>
-      <div
+    <section class="laptop:hidden w-full mx-auto mt-11">
+      <section
+        class="mobile-hero"
+        data-testid="mobile-tablet-hero-image"
+      ></section>
+      <section
         class="w-11/12 mx-auto flex flex-col justify-center content-center text-center space-y-8 mt-5 tablet:mt-24"
       >
         <h1
@@ -23,7 +28,7 @@
           Meet makes it easy to connect with others face-to-face virtually and
           collaborate across any device.
         </p>
-      </div>
+      </section>
       <div
         class="mx-auto flex flex-col justify-center content-center text-center space-y-3 mt-8 tablet:flex-row tablet:items-center tablet:space-y-0 tablet:w-1/2"
       >
@@ -38,17 +43,17 @@
           What is it?
         </button>
       </div>
-    </div>
+    </section>
 
     <!-- DESKTOP HERO -->
-    <div
-      class="hidden laptop:flex flex-row justify-center content-center w-full max-w-[1504px] mt-32 text-center mx-auto"
+    <section
+      class="hidden laptop:flex flex-row justify-center content-center w-full max-w-[1504px] mt-28 text-center mx-auto"
     >
-      <div
+      <section
         class="desktop-left-hero w-1/3"
         data-testid="desktop-left-hero-image"
-      ></div>
-      <div class="flex flex-col justify-between content-center w-1/3">
+      ></section>
+      <section class="flex flex-col justify-between content-center w-1/3">
         <h1
           class="text-meet-dark mx-auto font-black text-[64px] laptop:text-[50px] laptopl:text-[68px] leading-[64px]"
         >
@@ -61,7 +66,7 @@
           collaborate across any device.
         </p>
 
-        <div
+        <section
           class="mx-auto flex justify-center items-center text-center space-x-3 mt-8"
         >
           <button
@@ -74,54 +79,54 @@
           >
             What is it?
           </button>
-        </div>
-      </div>
-      <div
+        </section>
+      </section>
+      <section
         class="desktop-right-hero w-1/3"
         data-testid="desktop-right-hero-image"
-      ></div>
-    </div>
+      ></section>
+    </section>
     <!-- NUMBER 1 -->
-    <div
+    <section
       class="w-full mx-auto mt-20 flex flex-col justify-center content-center relative top-10 z-50"
     >
-      <div
+      <section
         class="border-l border-l-meet-gray h-[90px] absolute mb-[55px] left-1/2 opacity-75"
-      ></div>
-      <div
+      ></section>
+      <section
         class="border rounded-full border-solid border-meet-gray w-[56px] h-[56px] mt-[90px] mx-auto border-opacity-75 bg-meet-light"
       >
         <p class="text-meet-gray font-bold text-center mt-3 text-xl">01</p>
-      </div>
-    </div>
+      </section>
+    </section>
 
     <!-- IMAGES -->
-    <div
-      class="w-full flex flex-row flex-wrap content-center justify-center laptop:justify-between mt-20 mx-auto max-w-[1110px]"
+    <section
+      class="w-full flex flex-row flex-wrap content-center justify-center laptop:justify-between mt-20 mx-auto max-w-[1210px]"
     >
       <img
         v-for="image in images"
         :key="image"
         :src="image"
         alt="People enjoying our services"
-        class="rounded-lg w-1/3 laptop:w-1/4 mt-5 ml-5 laptop:max-w-[242px]"
+        class="rounded-lg w-1/3 laptop:w-1/4 mt-5 ml-5 laptop:max-w-[262px]"
       />
-    </div>
+    </section>
 
     <!-- TEXT -->
-    <div
-      class="mt-20 space-y-8 flex flex-col justify-center content-center text-center"
+    <section
+      class="mt-32 space-y-8 flex flex-col justify-center content-center text-center"
     >
-      <h3
+      <h2
         class="mx-auto uppercase text-meet-teal text-2xl tracking-[4px] font-bold laptop:w-1/3"
       >
         built for modern use
-      </h3>
-      <h2
+      </h2>
+      <h3
         class="text-meet-dark font-black text-5xl mx-auto w-10/12 tablet:w-9/12 laptop:w-1/3"
       >
         Smarter meetings all in one place
-      </h2>
+      </h3>
       <p
         class="w-10/12 tablet:w-9/12 mx-auto text-meet-gray font-medium text-[26px] leading-[36px] tablet:text-[20px] laptop:leading-[26px] laptop:text-[17px] laptop:w-1/3"
       >
@@ -129,36 +134,36 @@
         all in one workspace. Control who can join with invite-only team access,
         data encryption, and data export.
       </p>
-    </div>
+    </section>
 
     <!-- FOOTER -->
 
-    <div class="w-full mx-auto">
-      <div
+    <section class="w-full mx-auto">
+      <section
         class="w-full mx-auto mt-20 flex flex-col justify-center content-center relative top-10 z-50"
       >
-        <div
+        <section
           class="border-l border-l-meet-gray h-[90px] absolute mb-[55px] left-1/2 opacity-75"
-        ></div>
-        <div
+        ></section>
+        <section
           class="border rounded-full border-solid border-meet-gray w-[56px] h-[56px] mt-[90px] mx-auto border-opacity-75 bg-meet-light"
         >
           <p class="text-meet-gray font-bold text-center mt-3 text-xl">02</p>
-        </div>
-      </div>
+        </section>
+      </section>
       <footer
         class="w-full mx-auto flex flex-col justify-center content-center text-center bg-meet-teal relative bottom-0 left-0 z-20 text-meet-light"
       >
-        <div
-          class="footer pt-44 w-full laptop:pt-24 laptop:pb-20"
+        <section
+          class="footer pt-44 w-full laptop:pt-32 laptop:pb-20"
           data-testid="footer-background-class"
         >
-          <div
-            class="laptopl:flex laptopl:w-[1110px] laptopl:mx-auto laptopl:justify-between laptopl:text-left laptopl:my-auto"
+          <section
+            class="laptopl:flex laptopl:w-[1310px] laptopl:mx-auto laptopl:justify-between laptopl:text-left laptopl:my-auto"
           >
-            <h2 class="font-black text-[42px] mx-auto w-8/12 laptop:w-[350px]">
+            <h3 class="font-black text-[42px] mx-auto w-8/12 laptop:w-[350px]">
               Experience more together
-            </h2>
+            </h3>
             <p
               class="w-8/12 mx-auto font-medium text-[26px] leading-[36px] pt-7 tablet:text-[20px] laptop:leading-[26px] laptop:text-[18px] laptop:w-[350px]"
             >
@@ -166,16 +171,16 @@
               and group video sessions.
             </p>
             <button
-              class="w-6/12 border-0 rounded-full px-8 py-5 text-[16px] font-black text-meet-light bg-meet-purple mx-auto hover:bg-opacity-75 max-w-[193px] mt-7 mb-20"
+              class="w-6/12 max-w-[193px] border-0 rounded-full px-10 py-5 text-[16px] font-black text-meet-light bg-meet-purple mx-auto hover:bg-opacity-75 mt-7 mb-20"
             >
               Download
               <span class="text-meet-light-purple">v1.3</span>
             </button>
-          </div>
-        </div>
+          </section>
+        </section>
       </footer>
-    </div>
-  </div>
+    </section>
+  </section>
 </template>
 
 <script>
@@ -253,8 +258,9 @@ export default {
 @media only screen and (min-width: 1024px) {
   .footer::before {
     background-image: url("@/assets/MeetLandingPage/desktop/image-footer.jpg");
-    background-position: center;
-    height: 328px;
+    background-position: center 0%;
+    background-size: cover;
+    background-repeat: no-repeat;
   }
 }
 </style>
