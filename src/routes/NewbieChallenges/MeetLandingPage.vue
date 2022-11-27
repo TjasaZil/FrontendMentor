8bcd8dabc35a703ca21f07c69c4bc7ba8636ff8e
zuzexx <tjasa.zilavec@gmail.com>
2022-11-27 13:33:28 +0100

    .meetLandingPage tablet done

--- src/routes/NewbieChallenges/MeetLandingPage.vue
+++ src/routes/NewbieChallenges/MeetLandingPage.vue
@@ -8,30 +8,30 @@
     <div class="laptop:hidden w-full mx-auto mt-11">
       <div class="mobile-hero" data-testid="mobile-tablet-hero-image"></div>
       <div
-        class="w-11/12 mx-auto flex flex-col justify-center content-center text-center space-y-8 mt-5"
+        class="w-11/12 mx-auto flex flex-col justify-center content-center text-center space-y-8 mt-5 tablet:mt-24"
       >
         <h1
-          class="text-meet-dark w-9/12 mx-auto font-black text-[64px] leading-[64px]"
+          class="text-meet-dark w-9/12 tablet:w-6/12 tablet:text-[70px] mx-auto font-black text-[64px] leading-[64px]"
         >
           Group Chat for Everyone
         </h1>
         <p
-          class="w-11/12 mx-auto text-meet-gray font-medium text-[26px] leading-[36px]"
+          class="w-11/12 tablet:w-7/12 tablet:text-[20px] mx-auto text-meet-gray font-medium text-[26px] leading-[36px]"
         >
           Meet makes it easy to connect with others face-to-face virtually and
           collaborate across any device.
         </p>
       </div>
       <div
-        class="mx-auto flex flex-col justify-center content-center text-center space-y-3 mt-8"
+        class="mx-auto flex flex-col justify-center content-center text-center space-y-3 mt-8 tablet:flex-row tablet:items-center tablet:space-y-0 tablet:w-1/3 tablet:space-x-3"
       >
         <button
-          class="border-0 rounded-full px-4 py-5 text-xl font-black text-meet-light bg-meet-teal w-5/12 mx-auto hover:bg-opacity-75 max-w-[193px]"
+          class="border-0 rounded-full px-4 py-5 text-xl font-black text-meet-light bg-meet-teal mx-auto hover:bg-opacity-75 max-w-[263px] tablet:text-lg"
         >
           Download
           <span class="text-meet-light-teal">v1.3</span></button
         ><button
-          class="border-0 rounded-full px-4 py-5 text-xl font-black text-meet-light bg-meet-purple w-4/12 mx-auto hover:bg-opacity-75 max-w-[139px]"
+          class="border-0 rounded-full px-4 py-5 text-xl font-black text-meet-light bg-meet-purple mx-auto hover:bg-opacity-75 max-w-[189px]"
         >
           What is it?
         </button>
@@ -97,7 +97,7 @@
         :key="image"
         :src="image"
         alt="People enjoying our services"
-        class="rounded-lg w-1/3 mt-5 ml-5 max-w-[242px]"
+        class="rounded-lg w-1/3 mt-5 ml-5 laptop:max-w-[242px] tablet:max-w-[156px]"
       />
     </div>
     <div
@@ -106,11 +106,13 @@
       <h3 class="uppercase text-meet-teal text-2xl tracking-[4px] font-bold">
         built for modern use
       </h3>
-      <h2 class="text-meet-dark font-black text-5xl mx-auto w-8/12">
+      <h2
+        class="text-meet-dark font-black text-5xl mx-auto w-10/12 tablet:w-6/12"
+      >
         Smarter meetings all in one place
       </h2>
       <p
-        class="w-10/12 mx-auto text-meet-gray font-medium text-[26px] leading-[36px]"
+        class="w-10/12 tablet:w-9/12 mx-auto text-meet-gray font-medium text-[26px] leading-[36px] tablet:text-[20px]"
       >
         Send messages, share files, show your screen, and record your meetings —
         all in one workspace. Control who can join with invite-only team access,
@@ -132,18 +134,20 @@
         </div>
       </div>
       <footer
-        class="w-full mx-auto flex flex-col justify-center content-center text-center bg-meet-teal relative bottom-0 left-0 z-20 text-meet-light"
+        class="w-full mx-auto flex flex-col justify-center content-center text-center bg-meet-teal relative bottom-0 left-0 z-20 text-meet-light laptop:flex-row laptop:justify-between laptop:w-full laptop:content-center"
       >
         <div class="footer pt-44" data-testid="footer-background-class">
-          <h2 class="font-black text-5xl mx-auto w-8/12">
+          <h2 class="font-black text-5xl mx-auto w-8/12 laptop:w-1/3">
             Experience more together
           </h2>
-          <p class="w-8/12 mx-auto font-medium text-[26px] leading-[36px] pt-7">
+          <p
+            class="w-8/12 mx-auto font-medium text-[26px] leading-[36px] pt-7 tablet:text-[20px] laptop:w-1/3"
+          >
             Stay connected with reliable HD meetings and unlimited one-on-one
             and group video sessions.
           </p>
           <button
-            class="w-6/12 border-0 rounded-full px-4 py-5 text-xl font-black text-meet-light bg-meet-purple mx-auto hover:bg-opacity-75 max-w-[193px] mt-7 mb-20"
+            class="w-6/12 border-0 rounded-full px-5 py-5 text-xl font-black text-meet-light bg-meet-purple mx-auto hover:bg-opacity-75 max-w-[193px] mt-7 mb-20 laptop:w-1/3"
           >
             Download
             <span class="text-meet-light-purple">v1.3</span>
