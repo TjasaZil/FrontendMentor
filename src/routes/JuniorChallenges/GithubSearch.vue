0f3b65a7cc2b4c085ee430ca5dcaa98621a5927a
zuzexx <tjasa.zilavec@gmail.com>
2023-02-12 00:11:14 +0100

    .testing layout done

--- src/routes/JuniorChallenges/GithubSearch.vue
+++ src/routes/JuniorChallenges/GithubSearch.vue
@@ -8,30 +8,38 @@
       <div
         class="w-full flex justify-between items-center border-2 border-green-500"
       >
-        <h1>heading</h1>
+        <h1 data-testid="main-heading-test">devfinder</h1>
         <div class="flex justify-end items-center space-x-3">
-          <p>theme</p>
-          <button>btn</button>
+          <p data-testid="theme-text-test">theme</p>
+          <button data-testid="theme-btn-test">
+            <img
+              alt="theme button image"
+              src="../../assets/githubSearch/icon-sun.svg"
+            />
+          </button>
         </div>
       </div>
       <div
         class="border-2 border-white rounded-lg w-full flex justify-between p-2 items-center"
       >
-        <div>icon</div>
-        <div>input</div>
-        <button>search-btn</button>
+        <img
+          alt="search icon"
+          src="../../assets/githubSearch/icon-search.svg"
+        />
+        <input type="text" placeholder="Search GitHub username..." />
+        <button data-testid="search-btn-test">search-btn</button>
       </div>
       <div
         class="border-4 border-pink-400 rounded-xl w-full p-4 flex flex-col justify-end items-end space-y-8"
       >
         <div class="w-full flex justify-between align-top">
-          <p>profile image</p>
+          <p data-testid="profile-img-test">profile image</p>
           <div class="flex flex-col justify-center items-start text-left">
-            <h2>name</h2>
-            <h3>@username</h3>
-            <p>Profile bio</p>
+            <h2 data-testid="profile-name-test">name</h2>
+            <h3 data-testid="username-name-test">@username</h3>
+            <p data-testid="profile-bio-test">Profile bio</p>
           </div>
-          <p>date when joined</p>
+          <p data-testid="profile-date-test">Joined <span>date</span></p>
         </div>
         <div
           class="border-4 border-green w-4/6 max-w-[480px] flex flex-col justify-center items-center"
@@ -44,21 +52,25 @@
               :key="text"
               class="flex flex-col justify-center items-center"
             >
-              <h4>{{ text.heading }}</h4>
-              <p>{{ text.number }}</p>
+              <h4 data-testid="info-headings-test">{{ text.heading }}</h4>
+              <p data-testid="info-numbers-test">{{ text.number }}</p>
             </div>
           </div>
           <div
             class="border-4 border-yellow-300 w-full flex flex-row flex-wrap justify-center items-center"
+            data-testid="social-media-test"
           >
             <div
               v-for="data in datas"
               :key="data"
               class="w-1/2 space-x-5 mt-5 flex justify-start items-center"
             >
-              <div>
-                {{ data.src }}
-              </div>
+              <img
+                :src="data.src"
+                :alt="data.alt"
+                data-testid="social-icons-test"
+              />
+
               <div>{{ data.text }}</div>
             </div>
           </div>
@@ -69,20 +81,24 @@
 </template>
 
 <script>
+import Tag from "@/assets/githubSearch/icon-location.svg";
+import Company from "@/assets/githubSearch/icon-company.svg";
+import Twitter from "@/assets/githubSearch/icon-twitter.svg";
+import Website from "@/assets/githubSearch/icon-website.svg";
 export default {
   name: "GithubSearch",
   data() {
     return {
       texts: [
-        { heading: "heading1", number: 1 },
-        { heading: "heading2", number: 2 },
-        { heading: "heading3", number: 3 },
+        { heading: "Repos", number: 1 },
+        { heading: "Followers", number: 2 },
+        { heading: "Following", number: 3 },
       ],
       datas: [
-        { src: "img1", text: "text1" },
-        { src: "img2", text: "text4" },
-        { src: "img3", text: "text3" },
-        { src: "img4", text: "text4" },
+        { src: Tag, alt: "location", text: "text1" },
+        { src: Twitter, alt: "twitter", text: "text4" },
+        { src: Website, alt: "website", text: "text3" },
+        { src: Company, alt: "company", text: "text4" },
       ],
     };
   },
