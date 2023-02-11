39869cfee60249fc7fb8c636e19dab8c98952877
zuzexx <tjasa.zilavec@gmail.com>
2023-02-11 20:56:37 +0100

    .time to start testing

--- tests/unit/routes/JuniorChallenges/GithubSearch.test.js
+++ tests/unit/routes/JuniorChallenges/GithubSearch.test.js
@@ -1,3 +1,40 @@
-/*import "@testing-library/jest-dom";
-import { screen, render, waitFor } from "@testing-library/vue";
-import GithubSearch from "@/routes/JuniorChallenges/GithubSearch.vue";*/
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/vue";
+import GithubSearch from "@/routes/JuniorChallenges/GithubSearch.vue";
+describe("GithubSearch", () => {
+  describe("layout", () => {
+    it("has a heading", () => {
+      render(GithubSearch);
+      const heading = screen.queryByTestId("main-heading-test");
+      expect(heading).toBeVisible();
+    });
+    it("has a  heading with text devfinder", () => {});
+    it("has text next to theme button", () => {});
+    it("Has theme button", () => {});
+    it("has a search icon", () => {});
+    it("has an input", () => {});
+    it("has placeholder text in input", () => {});
+    it("has a search button", () => {});
+    it("has profile picture", () => {});
+    it("has profile name", () => {});
+    it("has username", () => {});
+    it("has profile bio", () => {});
+    it("has date joined", () => {});
+    it("has an information box", () => {});
+    it("generates 3 headings and 3 numbers in the info box", () => {});
+    it("has social media box", () => {});
+    it("has location info", () => {});
+    it("has location icon", () => {});
+    it("has twitter info", () => {});
+    it("has twitter icon", () => {});
+    it("has site info", () => {});
+    it("has site icon", () => {});
+    it("has home info", () => {});
+    it("has home icon", () => {});
+  });
+  describe("functionality", () => {
+    it("changes the theme button on click", () => {});
+    it("changes the theme text on click", () => {});
+    it("searches for user when the search button is clicked", () => {});
+  });
+});
