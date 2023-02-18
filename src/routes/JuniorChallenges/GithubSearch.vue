37ad92f5fac1e86a4eb11531216cf141afbaf8a9
zuzexx <tjasa.zilavec@gmail.com>
2023-02-18 22:27:12 +0100

    .

--- src/routes/JuniorChallenges/GithubSearch.vue
+++ src/routes/JuniorChallenges/GithubSearch.vue
@@ -1,27 +1,19 @@
 <template>
-  <div
-    class="font-['Space_Mono'] w-screen h-screen flex flex-col justify-start pt-10 tablet:pt-0 tablet:justify-center items-center bg-github-dark-black"
-  >
+  <div class="main" :class="{ dark: darkMode }">
     <div
       class="mx-auto flex flex-col justify-center items-center w-[327px] tablet:w-[573px] laptop:w-[730px] space-y-5"
     >
       <div class="w-full flex justify-between items-center">
-        <h1 data-testid="main-heading-test">devfinder</h1>
+        <h1 class="text-2xl font-semibold">devfinder</h1>
         <div class="flex justify-end items-center space-x-3">
-          <p data-testid="theme-text-test" v-if="this.darkMode">Light</p>
-          <p data-testid="theme-text-test" v-if="!this.darkMode">Dark</p>
-          <button data-testid="theme-btn-test">
+          <p class="theme-text">
+            {{ darkMode ? "Light" : "Dark" }}
+          </p>
+
+          <button class="toggle" @click="changeTheme">
             <img
-              v-if="this.darkMode"
-              alt="theme button image"
-              src="../../assets/githubSearch/icon-sun.svg"
-              @click="changeTheme"
-            />
-            <img
-              v-if="!this.darkMode"
-              @click="changeTheme"
-              alt="theme button image"
-              src="../../assets/githubSearch/icon-moon.svg"
+              :src="darkMode ? sun : moon"
+              :alt="darkMode ? 'sun' : 'moon'"
             />
           </button>
         </div>
@@ -36,70 +28,68 @@
         <input
           type="text"
           placeholder="Search GitHub username..."
-          class="w-full bg-github-dark-blue placeholder:text-github-dark-white placeholder:text-xs"
+          class="w-full bg-github-dark-blue placeholder:text-github-dark-white px-2 placeholder:text-xs"
           v-model="githubUser"
         />
         <button
-          data-testid="search-btn-test"
           @click="searchUser"
-          class="rounded-xl text-white bg-github-light-blue px-3 py-2"
+          class="rounded-xl text-white font-semibold bg-github-light-blue px-3 py-2"
         >
           Search
         </button>
       </div>
       <div
-        class="rounded-xl w-full p-4 flex flex-col justify-end items-end space-y-8 bg-github-dark-blue"
+        class="rounded-xl w-full p-4 flex flex-col justify-end items-end space-y-8 bg-github-dark-blue laptop:py-8"
       >
         <div class="w-full flex justify-between align-top space-x-6">
           <div
             class="w-11/12 mx-auto flex flex-col justify-between space-y-5 tablet:justify-start tablet:space-x-10 items-center laptop:items-center laptop:justify-between laptop:space-y-0"
           >
             <div
-              class="w-full mx-auto flex justify-between tablet:justify-start tablet:space-x-10 items-center laptop:items-start laptop:justify-between"
+              class="w-full mx-auto flex flex-col justify-between tablet:justify-start items-center laptop:items-start laptop:justify-between space-y-6"
             >
-              <img
-                :src="user.avatar_url"
-                alt="image icon"
-                class="rounded-full w-[70px] h-[70px] tablet:w-[117px] tablet:h-[117px]"
-                data-testid="profile-img-test"
-              />
               <div
-                class="w-full laptop:flex laptop:justify-between laptop:items-start"
+                class="w-full flex flex-row justify-center items-center space-x-4 tablet:space-x-10"
               >
-                <div class="flex flex-col justify-center items-start text-left">
-                  <h2 data-testid="profile-name-test">
-                    {{ user.name || "Not available" }}
-                  </h2>
-                  <h3
-                    data-testid="username-name-test"
-                    class="text-github-light-blue"
+                <img
+                  :src="user.avatar_url"
+                  alt="image icon"
+                  class="rounded-full w-[70px] h-[70px] tablet:w-[117px] tablet:h-[117px]"
+                />
+                <div
+                  class="w-full laptop:flex laptop:justify-between laptop:items-start"
+                >
+                  <div
+                    class="flex flex-col justify-center items-start text-left"
                   >
-                    @{{ user.login || "Not available" }}
-                  </h3>
+                    <h2 class="text-lg font-bold">
+                      {{ user.name || "Not available" }}
+                    </h2>
+                    <h3 class="text-github-light-blue text-sm">
+                      @{{ user.login || "Not available" }}
+                    </h3>
+                  </div>
+                  <p class="text-sm">
+                    Joined
+                    <span>
+                      {{
+                        user.created_at
+                          ? new Date(user.created_at).toLocaleDateString(
+                              "en-GB",
+                              {
+                                day: "numeric",
+                                month: "short",
+                                year: "numeric",
+                              }
+                            )
+                          : "Not available"
+                      }}
+                    </span>
+                  </p>
                 </div>
-                <p data-testid="profile-date-test">
-                  Joined
-                  <span>
-                    {{
-                      user.created_at
-                        ? new Date(user.created_at).toLocaleDateString(
-                            "en-GB",
-                            {
-                              day: "numeric",
-                              month: "short",
-                              year: "numeric",
-                            }
-                          )
-                        : "Not available"
-                    }}
-                  </span>
-                </p>
               </div>
             </div>
-            <p
-              data-testid="profile-bio-test"
-              class="mx-auto text-center laptop:text-left laptop:max-w-[480px]"
-            >
+            <p class="mx-auto text-left laptop:max-w-[480px] text-sm w-full">
               {{ user.bio || "Not available" }}
             </p>
           </div>
@@ -111,62 +101,45 @@
           <div
             class="b w-full flex justify-evenly items-center bg-github-dark-black py-3 rounded-xl"
           >
-            <div class="flex flex-col justify-center items-center">
-              <h4 data-testid="info-headings-test">Repos</h4>
-              <p data-testid="info-numbers-test">
+            <div class="repo-comp">
+              <h4 class="repo-text">Repos</h4>
+              <p class="repo-num">
                 {{ user.public_repos || "Not available" }}
               </p>
             </div>
-            <div class="flex flex-col justify-center items-center">
-              <h4 data-testid="info-headings-test">Followers</h4>
-              <p data-testid="info-numbers-test">
+            <div class="repo-comp">
+              <h4 class="repo-text">Followers</h4>
+              <p class="repo-num">
                 {{ user.followers || "Not available" }}
               </p>
             </div>
-            <div class="flex flex-col justify-center items-center">
-              <h4 data-testid="info-headings-test">Following</h4>
-              <p data-testid="info-numbers-test">
+            <div class="repo-comp">
+              <h4 class="repo-text">Following</h4>
+              <p class="repo-num">
                 {{ user.following || "Not available" }}
               </p>
             </div>
           </div>
           <div
             class="w-full flex flex-col tablet:flex-row tablet:flex-wrap items-start justify-center tablet:items-center"
-            data-testid="social-media-test"
           >
-            <div class="w-1/2 space-x-5 mt-5 flex justify-start items-center">
-              <img
-                src="@/assets/githubSearch/icon-location.svg"
-                alt="tag"
-                data-testid="social-icons-test"
-              />
+            <div class="info" :class="{ na: user.location === null }">
+              <img src="@/assets/githubSearch/icon-location.svg" alt="tag" />
 
               <div>{{ user.location || "Not available" }}</div>
             </div>
-            <div class="w-1/2 space-x-5 mt-5 flex justify-start items-center">
-              <img
-                src="@/assets/githubSearch/icon-twitter.svg"
-                alt="twitter"
-                data-testid="social-icons-test"
-              />
+            <div class="info" :class="{ na: user.twitter_username === null }">
+              <img src="@/assets/githubSearch/icon-twitter.svg" alt="twitter" />
 
               <div>{{ user.twitter_username || "Not available" }}</div>
             </div>
-            <div class="w-1/2 space-x-5 mt-5 flex justify-start items-center">
-              <img
-                src="@/assets/githubSearch/icon-website.svg"
-                alt="website"
-                data-testid="social-icons-test"
-              />
+            <div class="info" :class="{ na: user.blog === null }">
+              <img src="@/assets/githubSearch/icon-website.svg" alt="website" />
 
               <div>{{ user.blog || "Not available" }}</div>
             </div>
-            <div class="w-1/2 space-x-5 mt-5 flex justify-start items-center">
-              <img
-                src="@/assets/githubSearch/icon-company.svg"
-                alt="company"
-                data-testid="social-icons-test"
-              />
+            <div class="info" :class="{ na: user.company === null }">
+              <img src="@/assets/githubSearch/icon-company.svg" alt="company" />
 
               <div>{{ user.company || "Not available" }}</div>
             </div>
@@ -178,24 +151,19 @@
 </template>
 
 <script>
-import Tag from "@/assets/githubSearch/icon-location.svg";
-import Company from "@/assets/githubSearch/icon-company.svg";
-import Twitter from "@/assets/githubSearch/icon-twitter.svg";
-import Website from "@/assets/githubSearch/icon-website.svg";
+import sun from "../../assets/githubSearch/icon-sun.svg";
+import moon from "../../assets/githubSearch/icon-moon.svg";
 import axios from "axios";
 
 export default {
   name: "GithubSearch",
   data() {
     return {
+      sun,
+      moon,
       darkMode: true,
       githubUser: "",
-      datas: [
-        { src: Tag, alt: "location", text: "text1" },
-        { src: Twitter, alt: "twitter", text: "{{user.twitter_username}} " },
-        { src: Website, alt: "website", text: "text3" },
-        { src: Company, alt: "company", text: "text4" },
-      ],
+
       user: [],
     };
   },
@@ -218,10 +186,32 @@ export default {
     },
   },
   mounted() {
-    this.githubUser = "octocat";
+    this.githubUser = "zuzexx";
     this.searchUser();
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.main {
+  @apply font-['Space_Mono'] w-screen h-screen flex flex-col justify-start pt-10 tablet:pt-0 tablet:justify-center items-center bg-github-dark-black;
+}
+.theme-text {
+  @apply uppercase font-medium;
+}
+.repo-comp {
+  @apply flex flex-col justify-center items-center;
+}
+.repo-text {
+  @apply text-xs;
+}
+.repo-num {
+  @apply font-extrabold text-base;
+}
+.info {
+  @apply laptop:w-1/2 space-x-5 mt-5 flex justify-start items-center text-sm w-full;
+}
+.na {
+  color: #697c9a;
+}
+</style>
