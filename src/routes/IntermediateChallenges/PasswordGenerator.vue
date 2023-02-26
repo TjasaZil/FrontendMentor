2d679d682a6aac1968e42b9ec05bb368b3cc3598
zuzexx <tjasa.zilavec@gmail.com>
2023-02-26 19:36:10 +0100

    generates password

--- src/routes/IntermediateChallenges/PasswordGenerator.vue
+++ src/routes/IntermediateChallenges/PasswordGenerator.vue
@@ -3,16 +3,18 @@
     class="w-screen h-screen overflow-hidden flex flex-col justify-center items-center text-['JetBrains_Mono'] bg-password-black"
   >
     <div
-      class="border-2 border-pink-400 w-11/12 mx-auto flex flex-col justify-center items-center space-y-5"
+      class="w-11/12 mx-auto flex flex-col justify-center items-center space-y-5 max-w-[540px]"
     >
       <h1 class="text-password-gray capitalize">password generator</h1>
+      <!-- COPY PASSWORD -->
       <div
         class="flex flex-row justify-between items-center bg-password-dark w-full mx-auto p-3"
       >
         <input
           type="text"
           class="p-2 bg-transparent placeholder:text-password-grayish text-xl placeholder:text-xl text-password-grayish font-medium"
-          placeholder="vukvfguzoldsgbfčiS"
+          placeholder="password"
+          v-model="generatedPassword"
           readonly
         /><button>
           <img
@@ -21,14 +23,13 @@
           />
         </button>
       </div>
+      <!-- MAIN CONTAINER-->
       <div
-        class="w-full space-y-5 mx-auto border-2 border-amber-300 bg-password-dark flex flex-col justify-center items-center"
+        class="w-full space-y-5 mx-auto bg-password-dark flex flex-col justify-center items-center py-8"
       >
-        <div
-          class="border-2 border-green-400 w-11/12 mx-auto flex flex-col justify-center items-center"
-        >
+        <div class="w-11/12 mx-auto flex flex-col justify-center items-center">
           <div
-            class="flex flex-row justify-between items-center border-2 border-blue-500 w-full mx-auto p-3"
+            class="flex flex-row justify-between items-center w-full mx-auto p-3"
           >
             <label for="range" class="capitalize">Character length</label>
             <p>{{ lengthNumber }}</p>
@@ -42,55 +43,57 @@
             max="20"
           />
         </div>
+        <!-- CHECKBOXES -->
         <div
           class="w-full flex flex-col justify-start items-center text-password-grayish font-medium"
         >
           <div
-            class="flex flex-row justify-start items-center p-3 space-x-4 border-2 border-white w-11/12 mx-auto"
+            class="flex flex-row justify-start items-center p-3 space-x-4 w-11/12 mx-auto"
           >
-            <input type="checkbox" id="uppercase" /><label
+            <input type="checkbox" id="uppercase" v-model="isUppercase" /><label
               for="uppercase"
               class="capitalize"
               >include uppercase letters</label
             >
           </div>
           <div
-            class="flex flex-row justify-start items-center p-3 space-x-4 border-2 border-white w-11/12 mx-auto"
+            class="flex flex-row justify-start items-center p-3 space-x-4 w-11/12 mx-auto"
           >
-            <input type="checkbox" id="lowercase" checked /><label
+            <input type="checkbox" id="lowercase" v-model="isLowercase" /><label
               for="lowercase"
               class="capitalize"
               >include lowercase letters</label
             >
           </div>
           <div
-            class="flex flex-row justify-start items-center p-3 space-x-4 border-2 border-white w-11/12 mx-auto"
+            class="flex flex-row justify-start items-center p-3 space-x-4 w-11/12 mx-auto"
           >
-            <input type="checkbox" id="numbers" /><label
+            <input type="checkbox" id="numbers" v-model="isNumbers" /><label
               for="numbers"
               class="capitalize"
               >include numbers</label
             >
           </div>
           <div
-            class="flex flex-row justify-start items-center p-3 space-x-4 border-2 border-white w-11/12 mx-auto"
+            class="flex flex-row justify-start items-center p-3 space-x-4 w-11/12 mx-auto"
           >
-            <input type="checkbox" id="symbols" /><label
+            <input type="checkbox" id="symbols" v-model="isSymbol" /><label
               for="symbols"
               class="capitalize"
               >include symbols</label
             >
           </div>
         </div>
+        <!-- STRENGTH -->
         <div
-          class="bg-password-black w-11/12 border-2 border-orange-300 p-3 mx-auto flex flex-row justify-between items-center"
+          class="bg-password-black w-11/12 p-3 mx-auto flex flex-row justify-between items-center"
         >
           <h2 class="uppercase text-password-gray text-lg font-semibold">
             strength
           </h2>
           <div class="flex flex-row justify-center items-center space-x-3">
             <h2 class="font-semibold uppercase text-password-grayish text-lg">
-              medium
+              {{ this.strengthValue }}
             </h2>
             <div class="flex flex-row justify-center items-center space-x-2">
               <div class="border-2 border-password-grayish h-7 w-3"></div>
@@ -100,8 +103,10 @@
             </div>
           </div>
         </div>
+        <!-- BUTTON-->
         <button
           class="w-11/12 bg-password-green text-password-dark py-3 text-lg font-medium mx-auto"
+          @click="generatePassword()"
         >
           <div
             class="w-full mx-auto flex space-x-4 items-center justify-center"
@@ -114,6 +119,9 @@
             />
           </div>
         </button>
+        <p class="text-lg font-semibold underline text-password-red">
+          {{ this.errorMessage }}
+        </p>
       </div>
     </div>
   </div>
@@ -125,8 +133,67 @@ export default {
   data() {
     return {
       lengthNumber: 10,
-      password: "qwertzuiopasdfghjklyxcvbnm",
+      password: "",
+      generatedPassword: "PTx1f5DaFX",
+      isUppercase: false,
+      isLowercase: true,
+      isNumbers: false,
+      isSymbol: false,
+      errorMessage: "",
+      strengthValue: "Medium",
     };
+  },
+  methods: {
+    generatePassword() {
+      this.getPassword();
+      this.checked();
+      this.checkPasswordStrength();
+      this.generatedPassword = "";
+      for (let i = 1; i <= this.lengthNumber; i++) {
+        let k = Math.floor(Math.random() * this.password.length);
+        this.generatedPassword += this.password[k];
+      }
+    },
+    getPassword() {
+      if (
+        !this.isUppercase &&
+        !this.isLowercase &&
+        !this.isNumbers &&
+        !this.isSymbol
+      )
+        this.errorMessage = "Please check at least one checkbox";
+      if (this.lengthNumber == 0)
+        this.errorMessage = "The Password length can not be 0";
+    },
+    checked() {
+      this.password = "";
+      if (this.isUppercase) this.password += "QWERTZUIOPASDFGHJKLYXCVBNM";
+      if (this.isLowercase) this.password += "qwertzuiopasdfghjklyxcvbnm";
+      if (this.isNumbers) this.password += "1234567890";
+      if (this.isSymbol) this.password += "!#$%&/()=?*-_<>.;[]{}@";
+    },
+    checkPasswordStrength() {
+      if (this.lengthNumber < 8 || !/[A-Z]/.test(this.password)) {
+        this.strengthValue = "Too weak";
+      } else if (this.lengthNumber < 11) {
+        if (!/[a-z]/.test(this.password) || !/\d/.test(this.password)) {
+          this.strengthValue = "Weak";
+        } else {
+          this.strengthValue = "Medium";
+        }
+      } else {
+        if (
+          !/[a-z]/.test(this.password) ||
+          !/[A-Z]/.test(this.password) ||
+          !/\d/.test(this.password) ||
+          !/[^A-Za-z0-9]/.test(this.password)
+        ) {
+          this.strengthValue = "Medium";
+        } else {
+          this.strengthValue = "Strong";
+        }
+      }
+    },
   },
 };
 </script>
