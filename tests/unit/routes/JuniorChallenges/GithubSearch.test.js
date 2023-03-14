3fc50f8b3fdea9d0a83e872003c39f970b8bb63a
zuzexx <tjasa.zilavec@gmail.com>
2023-03-14 05:51:37 +0100

    nastavljeni github search testi

---
+++ tests/unit/routes/JuniorChallenges/GithubSearch.test.js
@@ -0,0 +1,31 @@
+import "@testing-library/jest-dom";
+//import { screen, render } from "@testing-library/vue";
+//import { userEvent } from "@testing-library/user-event";
+//import GithubSearch from "@/routes/JuniorChallenges/GithubSearch.vue";
+
+describe("GithubSearch", () => {
+  describe("layout", () => {
+    it("has a logo", () => {});
+    it("has a theme toggle", () => {});
+    it("has input field", () => {});
+    it("has search button", () => {});
+    it("renders avatar img", () => {});
+    it("renders login", () => {});
+    it("renders bio", () => {});
+    it("renders blog", () => {});
+    it("renders company", () => {});
+    it("renders created_at", () => {});
+    it("renders followers", () => {});
+    it("renders folowing", () => {});
+    it("renders location", () => {});
+    it("renders name", () => {});
+    it("renders repo", () => {});
+    it("renders twitter", () => {});
+  });
+  describe("functionality", () => {
+    it("changes text when we click on theme toggle", () => {});
+    it("changes img when clicked on theme toggle", () => {});
+    it("calls theme function when clicked on theme toggle", () => {});
+    it("fetches data when clicked on a search button", () => {});
+  });
+});
